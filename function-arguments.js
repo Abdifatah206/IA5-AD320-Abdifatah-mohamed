6e24ad2b8fe7a50b8c6e29dc73fba8567c4ee859
Abdifatah206 <46364693+Abdifatah206@users.noreply.github.com>
2019-02-13 23:33:47 -0800

    add
    add

---
+++ function-arguments.js
@@ -0,0 +1,7 @@
+function math(a1 = 53,a2 = 61, a3 = 67 ){
+	return a1+a2*67;
+	
+	
+	
+}
+console.log(math())
---
+++ if-statement.js
@@ -0,0 +1,9 @@
+var fruit =('orange');
+//fruit = fruit.tostring();
+if(fruit > 5){
+console.load('The fruit name has  five characters .');
+}
+ else{
+	console.log('The fruit name has more than five characters.');
+	
+}
